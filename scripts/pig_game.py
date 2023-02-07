4ca941ec75bb39c5b6c8579ab249bfb1e48413a6
Jonathan <yonilevi0013@gmail.com>
2023-02-07 21:44:55 +0200

    fixing python script

--- scripts/pig_game.py
+++
@@ -1,8 +0,0 @@
-import subprocess
-
-try:
-    subprocess.run(["kubectl delete -f k8s/pigGame/pig_game_deploy.yaml"])
-    subprocess.run(["kubectl delete -f k8s/pigGame/pig_game_service.yaml"])
-except:
-     subprocess.run(["kubectl create -f k8s/pigGame/pig_game_deploy.yaml"])
-     subprocess.run(["kubectl create -f k8s/pigGame/pig_game_service.yaml"])
